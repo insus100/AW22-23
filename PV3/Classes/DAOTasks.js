a92d1d74928a6666b118f1fe5356893ea6b5465c
insus100 <37269595+insus100@users.noreply.github.com>
2022-11-03 18:35:40 +0100

    fixes

--- PV3/Classes/DAOTasks.js
+++ PV3/Classes/DAOTasks.js
@@ -15,7 +15,7 @@ class DAOTasks {
                     else {
                         if (rows.length === 0) callback(new Error("No existe el usuario"));
                         else {
-                            return rows[0].idUser;
+                            callback(null, rows[0].idUser);
                         }
                     }
                 });
@@ -27,34 +27,35 @@ class DAOTasks {
         this.pool.getConnection((err, connection) => {
             if(err) callback(new Error("Error de conexión a la base de datos"));
             else {
-                const idUser = this.getUserIdFromEmail(email, (err) => {
-                    console.log(err);
-                })
-
-                if(idUser != undefined){
-                    /*SELECT idUser, aw_tareas_tareas.idTareas, hecho, aw_tareas_tareas.texto, aw_tareas_etiquetas.texto FROM aw_tareas_user_tarea JOIN aw_tareas_tareas ON aw_tareas_user_tarea.idTarea = aw_tareas_tareas.idTareas JOIN aw_tareas_tareas_etiquetas ON aw_tareas_tareas.idTareas = aw_tareas_tareas_etiquetas.idTarea JOIN aw_tareas_etiquetas ON aw_tareas_etiquetas.idEtiqueta = aw_tareas_tareas_etiquetas.idEtiqueta WHERE aw_tareas_user_tarea.idUser = 1;*/
-                    connection.query(`SELECT * FROM aw_tareas_user_tarea JOIN aw_tareas_tareas ON aw_tareas_user_tarea.idTarea = aw_tareas_tareas.idTareas WHERE aw_tareas_user_tarea.idUser = ${idUser}`,
-                    (err, rows) => {
-                        connection.release();
-                        if(err) callback(new Error("Error de acceso a la base de datos"));
-                        else {
-                            let tasks = [];
-                            rows.forEach((tarea) => {
-                                if(tasks[tarea.idTarea] === undefined){
-                                    tasks[tarea.idTarea] = {
-                                        idTareas: tarea.idTarea,
-                                        hecho: tarea.hecho,
-                                        texto: tarea.texto,
-                                        tags: [tarea.etiqueta]
-                                    };
+                this.getUserIdFromEmail(email, (err, idUser) => {
+                    if(err) console.log(err);
+                    else {
+                        if(idUser !== undefined){
+                            /*SELECT idUser, aw_tareas_tareas.idTareas, hecho, aw_tareas_tareas.texto, aw_tareas_etiquetas.texto FROM aw_tareas_user_tarea JOIN aw_tareas_tareas ON aw_tareas_user_tarea.idTarea = aw_tareas_tareas.idTareas JOIN aw_tareas_tareas_etiquetas ON aw_tareas_tareas.idTareas = aw_tareas_tareas_etiquetas.idTarea JOIN aw_tareas_etiquetas ON aw_tareas_etiquetas.idEtiqueta = aw_tareas_tareas_etiquetas.idEtiqueta WHERE aw_tareas_user_tarea.idUser = 1;*/
+                            connection.query(`SELECT * FROM aw_tareas_user_tarea JOIN aw_tareas_tareas ON aw_tareas_user_tarea.idTarea = aw_tareas_tareas.idTareas WHERE aw_tareas_user_tarea.idUser = ${idUser}`,
+                            (err, rows) => {
+                                connection.release();
+                                if(err) callback(new Error("Error de acceso a la base de datos"));
+                                else {
+                                    let tasks = [];
+                                    rows.forEach((tarea) => {
+                                        if(tasks[tarea.idTarea] === undefined){
+                                            tasks[tarea.idTarea] = {
+                                                idTareas: tarea.idTarea,
+                                                hecho: tarea.hecho,
+                                                texto: tarea.texto,
+                                                tags: [tarea.etiqueta]
+                                            };
+                                        }
+                                        else tasks[tarea.idTarea].tags.push(tarea.etiqueta); 
+                                    });
+                                    tasks = tasks.filter(t => t != undefined);//quitar objetos vacios del array;
+                                    callback(null, tasks);
                                 }
-                                else tasks[tarea.idTarea].tags.push(tarea.etiqueta); 
-                            });
-
-                            tasks = tasks.filter(t => t != undefined);
+                            })
                         }
-                    })
-                }
+                    }
+                });
             }
         });
     }
@@ -63,35 +64,33 @@ class DAOTasks {
         this.pool.getConnection((err, connection) => {
             if(err) callback(new Error("Error de conexión a la base de datos"));
             else{
-                let taskId;
                 connection.query(`INSERT INTO aw_tareas_tareas VALUES (${task.text})`, 
                 (err, result) => {
                     connection.release(); //TODO REVISAR
                     if(err) callback(new Error("Error de acceso a la base de datos"));
                     else {
-                        taskId = result.insertId;
+                        const taskId = result.insertId;
                         task.tags.forEach((t) => {
                             connection.query(`INSERT INTO aw_tareas_tareas_etiquetas VALUES (${taskId}), (${t.idEtiqueta})`, 
                             (err, result) => {
                                 if(err) callback(new Error("Error de acceso a la base de datos"));
-                                
-                            })
-                        })
+                            });
+                        });
+
+                        if(taskId != undefined){
+                            this.getUserIdFromEmail(email, (err, idUser) => {
+                                if(err) console.log(err);
+                                else {
+                                    connection.query(`INSERT INTO aw_tareas_user_tareas VALUES (${idUser}), (${taskId})`,
+                                    (err, result) => {
+                                        connection.release();
+                                        if(err) callback(new Error("Error de acceso a la base de datos"));
+                                    });
+                                }
+                            });
+                        }
                     }
                 });
-
-                if(taskId != undefined){
-                    const idUser = this.getUserIdFromEmail(email, (err) => {
-                        console.log(err);
-                    });
-                    
-                    connection.query(`INSERT INTO aw_tareas_user_tareas VALUES (${idUser}), (${taskId})`,
-
-                    (err, result) => {
-                        connection.release();
-                        if(err) callback(new Error("Error de acceso a la base de datos"));
-                    })
-                }
             }
         });
     }
@@ -113,15 +112,15 @@ class DAOTasks {
         this.pool.getConnection((err, connection) => {
             if(err) callback(new Error("Error de conexión a la base de datos"));
             else{
-                const idUser = this.getUserIdFromEmail(email, (err) => {
-                    console.log(err);
-                });
-
-                connection.query(`DELETE FROM aw_tareas_user_tareas WHERE hecho=1 AND idUser=${idUser}`, 
-                
-                (err, result) => {
-                    connection.release();
-                    if(err) callback(new Error("Error de acceso a la base de datos"));
+                this.getUserIdFromEmail(email, (err, idUser) => {
+                    if(err) console.log(err);
+                    else {
+                        connection.query(`DELETE FROM aw_tareas_user_tareas WHERE hecho=1 AND idUser=${idUser}`,
+                        (err, result) => {
+                            connection.release();
+                            if(err) callback(new Error("Error de acceso a la base de datos"));
+                        });
+                    }
                 });
             }
         });
