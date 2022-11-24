a905025a47589ecf911bcaa208f71030c8cb0385
smarcitllach <smarci01@ucm.es>
2022-11-24 17:45:04 +0100

    eyo kelouke pv5

--- PV4/app.js
+++ PV4/app.js
@@ -1,24 +1,53 @@
 "use strict";
 const config = require("./config");
 const { DAOTasks } = require("./DAOTasks");
-const { createTask } = require("./utils");
+const { DAOUsers } = require("./DAOUsers");
+const { createTask } = require("./utils/utils");
 const path = require("path");
 const mysql = require("mysql");
 const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
+const session = require("express-session");
+const mysqlSession = require("express-mysql-session");
+const { response } = require("express");
+const MySQLStore = mysqlSession(session);
+const { isAuthorized, isNotAuthorized } = require("./utils/auth");
+const sessionStore = new MySQLStore({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "aw_2022"
+})
 // Crear un servidor Express.js
 const app = express();
 // Crear un pool de conexiones a la base de datos de MySQL
 const pool = mysql.createPool(config.mysqlConfig);
 // Crear una instancia de DAOTasks
 const daoT = new DAOTasks(pool);
+const daoU = new DAOUsers(pool);
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //ponemos el directorio public como estatico
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    if(req.session && req.session.currentUser){
+        app.locals.user = req.session.currentUser;
+    }
+    next();
+});
+
+const middlewareSession = session({
+    saveUninitialized: false,
+    secret: "hytvr4vf",
+    resave: false,
+    store: sessionStore
+});
+
+app.use(middlewareSession);
 
 // Arrancar el servidor
 app.listen(config.port, function(err) {
@@ -30,7 +59,14 @@ app.listen(config.port, function(err) {
     }
 });
 
-app.get('/', function(req, res) {
+app.get("/reset", function(request, response) {
+    response.status(200);
+    request.session.contador = 0;
+    response.type("text/plain");
+    response.end("Has reiniciado el contador");
+});
+
+app.get('/', isAuthorized, function(req, res) {
     daoT.getAllTasks('aitor.tilla@ucm.es', (err, tasks) => {
         if(err) console.log(err);
         else {
@@ -40,7 +76,7 @@ app.get('/', function(req, res) {
     });
 });
 
-app.get('/finish/:id', (req, res) => {
+app.get('/finish/:id', isAuthorized, (req, res) => {
     const idTarea = parseInt(req.params.id);
     if(idTarea && !isNaN(idTarea)) {
         daoT.markTaskDone(idTarea, (err) => {
@@ -52,7 +88,7 @@ app.get('/finish/:id', (req, res) => {
     }
 });
 
-app.get('/deleteCompleted', (req, res) => {
+app.get('/deleteCompleted', isAuthorized, (req, res) => {
     daoT.deleteCompleted('aitor.tilla@ucm.es', (err) => {
         if(err) console.log(err);
         else {
@@ -62,7 +98,7 @@ app.get('/deleteCompleted', (req, res) => {
     
 });
 
-app.post('/addTask', (req, res) => {
+app.post('/addTask', isAuthorized, (req, res) => {
     if(req.body.tarea && req.body.tarea.length > 0) {
         const task = createTask(req.body.tarea);
         console.log("req.body", req.body, "\n", JSON.stringify(task));
@@ -80,4 +116,42 @@ app.post('/addTask', (req, res) => {
             }
         });
     } 
+});
+
+app.get('/login', (req, res) => {
+    if(req.session && req.session.currentUser){
+        res.redirect("/");
+    }
+    else{
+        res.render(path.join(__dirname, 'views/login'), {mensaje : ""});
+    }
+});
+
+app.post("/login", isNotAuthorized, function(req, res) {
+    if(req.body.email && req.body.password){
+        daoU.isUserCorrect(req.body.email,
+            req.body.password, function (err, ok) {
+                if(err){
+                    console.log("/login", err)
+                    res.status(500);
+                    res.render(path.join(__dirname, 'views/login'), {mensaje : "Error interno de acceso a la base de datos"});
+                }
+                else if(ok){
+                    req.session.currentUser = req.body.email;
+                    res.redirect("/");
+                }
+                else{
+                    res.status(200);
+                    res.render(path.join(__dirname, 'views/login'), {mensaje : "Email y/o contraseña no válidos"});
+                }
+            })
+    }
+    else{
+        res.render(path.join(__dirname, 'views/login'), {mensaje : "Introduzca todos los datos"});
+    }
+});
+
+app.get("/logout", isAuthorized, function(req, res){
+    req.session.destroy();
+    res.redirect("/login");
 });
